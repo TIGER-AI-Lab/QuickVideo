1f2de4ebeaf872e1753b6245f375657fd6abe3a9
Dongfu <dongfu.jdf@gmail.com>
2025-04-02 14:29:01 -0400

    update

--- lvu/models/qwen25_vl.py
+++ lvu/models/qwen25_vl.py
@@ -134,7 +134,7 @@ def smart_nframes(
     assert not ("fps" in ele and "nframes" in ele), "Only accept either `fps` or `nframes`"
     if "nframes" in ele:
         nframes = round_by_factor(ele["nframes"], FRAME_FACTOR)
-        nframes = min(max(nframes, min_frames), total_frames) # add this line for safety
+        nframes = min(nframes, total_frames) # add this line for safety
     else:
         fps = ele.get("fps", FPS)
         min_frames = ceil_by_factor(ele.get("min_frames", FPS_MIN_FRAMES), FRAME_FACTOR)
